1a0da34ebd37277700195b10d25c87132a2af0b9
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-05-17 21:23:23 -0600

    agent implemented

--- internal/bund/agent.go
+++ internal/bund/agent.go
@@ -1,15 +1,27 @@
 package bund
 
 import (
-	"fmt"
+	"github.com/nats-io/nats.go"
 	"github.com/pieterclaerhout/go-log"
 )
 
-
+func NRBundAgent(m *nats.Msg) {
+	msg := UnMarshal(m.Data)
+	if msg == nil {
+		log.Error("Invalid packet received")
+	}
+	IfSTOP(msg)
+	if msg.PktKey == "Agitator" && len(msg.Value) > 0 {
+		BundEvalExpression(string(msg.Value))
+	}
+}
 
 func Agent() {
 	Init()
 	InitEtcdAgent("agent")
-	fmt.Println(EtcdGetItems())
+	UpdateLocalConfigFromEtcd()
+	InitNatsAgent()
 	log.Debugf("[ NRBUND ] bund.Agent(%v) is reached", ApplicationId)
+	NatsRecv(NRBundAgent)
+	Loop()
 }
